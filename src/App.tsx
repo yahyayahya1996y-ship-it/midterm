f0dde41ea5bbe438b0e505a196146d12ae1da496
yahya <yahya.yahya.1996y@gmail.com>
2026-05-08 10:52:45 +0300

    Go to the new branch named dev create 3 files in components footer nav slidebar,then i use Aliosing with chaging in vite confi and tsconfigapp.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import MainLayout from "./layouts/MainLayout";
-import ProtectedRoute from "./components/ProtectedRoute";
-
+import Login from "@/pages/Login";
+import Dashboard from "@/pages/Dashboard";
+import MainLayout from "@/layouts/MainLayout";
+import ProtectedRoute from "@/components/ProtectedRoute";
 function App() {
   return (
     <BrowserRouter>
